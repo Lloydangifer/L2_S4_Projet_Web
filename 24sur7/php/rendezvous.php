7830a2b4060f054f9dd9f3123be1cf3b0ee20c94
Lloydangifer <vmanrique@netcourrier.com>
2015-04-13 16:41:07 +0200

    maj rendezvous

--- 24sur7/php/rendezvous.php
+++ 24sur7/php/rendezvous.php
@@ -1,4 +1,15 @@
-<?php include('bibli_24sur7.php');
+<?php
+include('bibli_24sur7.php');
+function lsvml_nouvelle_saisie(){
+	echo   '<p>Nouvelle saisie</p>',
+        '<hr>',
+        '<p>Libellé:</p>',
+        '<p>Date:<select name="date"><option value=1>1</option><option value=2>2</option></select></p>',
+        '<p>Catégorie<p>',
+        '<p>Horaire Début</p>',
+        '<p>Horaire Fin</p>',
+        '<p>ou</p>';
+}
 lsvm_html_head('24sur7 | Rendez-Vous');
 lsvm_html_bandeau('RDV');
 echo '<section id="bcContenu">',
@@ -15,8 +26,8 @@ echo		'<section id="categories">',
 			'</section>',
 		'</aside>',
 		'<section id="bcCentre">',
-			'Ici : bloc avec le détail des rendez-vous de la semaine du 9 au 15 février 2015',
-		'</section>',
+    lsvml_nouvelle_saisie();
+	echo	'</section>',
 	'</section>';
 lsvm_html_pied();
 ?>
