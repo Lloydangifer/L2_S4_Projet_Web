83a99e8aaff0ab4bb07d53d77b6cdb163840944c
Lloydangifer <vmanrique@netcourrier.com>
2015-04-15 16:40:51 +0200

    maj rdv

--- 24sur7/php/rendezvous.php
+++ 24sur7/php/rendezvous.php
@@ -1,14 +1,22 @@
 <?php
 include('bibli_24sur7.php');
 function lsvml_nouvelle_saisie(){
-	echo   '<p>Nouvelle saisie</p>',
-        '<hr>',
-        '<p>Libellé:</p>',
-        '<p>Date:<select name="date"><option value=1>1</option><option value=2>2</option></select></p>',
+	echo	'<p>Nouvelle saisie</p>',
+			'<hr>',
+			'<form method=POST action="rendezvous.php">',
+			'<p>Libellé: <input type="text" name="libelle"></p>',
+			'<p>Date:<input type="number" name="selDate_j" min="1" max="31" value="1" size="2" required>
+					 <input type="number" name="selDate_m" min="1" max="12" value="1" required>
+					 <input type="number" name="selDate_a" min="2015" value="2015" required></p>',
         '<p>Catégorie<p>',
-        '<p>Horaire Début</p>',
-        '<p>Horaire Fin</p>',
-        '<p>ou</p>';
+        '<p>Horaire Début:  <input type="number" name="heuredeb" min="0" max="23" value="8" required>:
+							<input type="number" name="minutesdeb" min="00" max="59" value="00" required></p>',
+        '<p>Horaire Fin: <input type="number" name="heurefin" min="0" max="23" value="8" required>:
+						 <input type="number" name="minutesfin" min="00" max="59" value="00" required></p>',
+        '<p>Ou <input type="checkbox" name="journee" id="journee" value="entiere"><label for="journee"> Evénement sur une journée</label></p>',
+        '<input type="submit" name="btnEnvoi" value="Ajouter"><input type="submit" name="btnAnnule" value="Annuler">',
+        '<p><a href="javascript:history.back()">Retour à l\'agenda</a></p>',
+        '</form>';
 }
 lsvm_html_head('24sur7 | Rendez-Vous');
 lsvm_html_bandeau('RDV');
